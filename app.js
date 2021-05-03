50d86a9c5146c4615900cc7ad16f9bce57bf71e8
changu100 <blue_rose17@naver.com>
2021-05-04 04:30:24 +0900

    체팅서버 작업완료

--- app.js
+++ app.js
@@ -10,6 +10,7 @@ var usersRouter = require('./routes/users');
 
 var app = express();
 var http = require('http').Server(app);
+var io = require('socket.io')(http);
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
@@ -40,6 +41,30 @@ app.use(function(err, req, res, next) {
   res.status(err.status || 500);
   res.render('error');
 });
+
+var count = 1;
+io.on('connection',function(socket){
+  console.log('user connected : ', socket.id);
+  var name = "익명" + count++;
+  socket.name = name;
+  io.to(socket.id).emit('create name', name);
+  io.emit('new_connect',name);
+
+	socket.on('disconnect', function(){   // 채팅방 접속이 끊어졌을 때 - 2
+	  console.log('user disconnected: '+ socket.id + ' ' + socket.name);
+	  io.emit('new_disconnect', socket.name);
+	});
+  
+  socket.on('send message', function(name, text){   // 메세지를 보냈을 때 - 3
+		var msg = name + ' : ' + text;
+		if(name != socket.name)   // 닉네임을 바꿨을 때 
+			io.emit('change name', socket.name, name);
+		socket.name = name;
+    	console.log(msg);
+    	io.emit('receive message', msg);
+	});
+})
+
 
 http.listen(3000,function(){
   console.log('server on...');
