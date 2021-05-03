f520b16db814cecf4544756ddbd5d72efe1463ff
changu100 <blue_rose17@naver.com>
2021-05-04 03:56:28 +0900

    routing 개념익히기

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('chat', { title: 'chatStart' });
+  res.render('chat', { title: 'index' });
 });
 
 module.exports = router;
